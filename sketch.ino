99aa2f53580a81d5e17ccc11713c60b7ab39aae3
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-11 12:48:35 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,18 +1,43 @@
 #include <TM1637.h>
 #include <Wire.h>
-int counter = 0;
+
 int buttonPin = 26;
-int buttonState;
+int lastState = LOW;
+int counter = 0;
+int ledPin = 22;
+int buzzerPin = 19;
 void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
   display.init();
   display.set(7);
-  display.display();
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW)
-
-  disp
-
-}
+  
+  int buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH && lastState == LOW){
+  counter += 1;
+  Serial.print(counter);
+  delay(50);
+ 
+  }
+   if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);     
+    tone(buzzerPin, 262);         
+  } else {
+    digitalWrite(ledPin, LOW);      
+    noTone(buzzerPin);             
+  }
+ lastState = buttonState;
+ if (counter < 10){
+  display(0, counter);
+ }
+ else if ( 10 =< counter <100){
+  display(2, counter / 10);
+  display(3, counter % 10);
+ }
+ }
